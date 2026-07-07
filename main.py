4837fe1b23b4f6b2f814010d9208392d300dcbc8
WikingWarr10r <110036601+TheoGroves@users.noreply.github.com>
2026-07-07 22:11:24 +0100

    Chore: Cleanup

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import time
 
-from core import Renderer, RenderPipeline, InputManager, Scene, AssetManager, Engine, Material
+from core import Scene, AssetManager, Engine, Material
 from core.components import MeshRenderer, Rigidbody, PlayerController, Input, CapsuleCollider, Transform, Camera, MeshCollider
-from core.systems import CollisionSystem, TransformSystem, MeshRendererSystem, InputSystem, PlayerControllerSystem, CameraSystem, MeshColliderSystem
+from core.systems import CollisionSystem, TransformSystem, MeshColliderSystem
 from rendering import RenderEngine, Camera, PlayerController, Mat4, Vec3, PBRMaterial, SkyboxMaterial
 
 # Engine Variables
